c73f40d7a9ef38fe2c4d73e90d8eba09c8813522
Vitus <tunv.b22cn748@stu.ptit.edu.vn>
2025-09-12 18:03:28 +0700

    BuildingMap

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,51 +1,62 @@
-import { _decorator, Component, instantiate, JsonAsset, Mesh, Node, Prefab, resources } from 'cc';
+import { _decorator, Component, instantiate, JsonAsset, Mesh, Node, Prefab, resources, Vec3 } from 'cc';
 const { ccclass, property } = _decorator;
 
 
 
 @ccclass('GameManager')
 export class GameManager extends Component {
-    @property(Prefab) 
-    legoFlatPrefab: Prefab = null;
+    @property([Prefab]) 
+    BlockGroup: Prefab[] = [];
+
+    @property([Prefab])
+    Blockades: Prefab[] = [];
+
 
 
 	start() {
-        this.loadLevel(6)
+        this.loadLevel(1)
 	}
 
-    static blockType: Record<number, string> = {
-        0: "1",
-        1: "8",
-        2: "7",
-        3: "6",
-        4: "6",
-        5: "2",
-        6: "3",
-        7: "4",
-        8: "5",
-        9: "9",
-        10: "9",
-        11: "10"
-    };
+    getBlockGroupByIndex(index: number): Prefab | null {
+        if (index >= 0 && index < this.BlockGroup.length) {
+            return this.BlockGroup[index];
+        }
+        return null;
+    }
+
+    getBlockadesByIndex(index: number): Prefab | null {
+        if (index >= 0 && index < this.Blockades.length) {
+            return this.Blockades[index];
+        }
+        return null;
+    }
 
 	loadLevel(index : number) {
-        console.log(this.legoFlatPrefab);
 		const name = `Level ${index}`;
 		resources.load(`level/${name}`, (err: Error, jsonAsset : JsonAsset) => {
 			if (!err) {
-                const node = instantiate(this.legoFlatPrefab)
-                const map = jsonAsset.json.levelBlockGroupsData.blockGroupDatas;
+                let map = jsonAsset.json.levelBlockGroupsData.blockGroupDatas;
                 map.forEach(block => {
-                    const legoNode = node.getChildByName(`Lego_Flat_${GameManager.blockType[block.blockGroupType]}`)
-                    if (legoNode) {
-                        const legoClone = instantiate(legoNode);
-                        legoClone.setPosition(block.position.x, block.position.y, block.position.z);
-                        legoClone.setRotationFromEuler(block.rotation.x, 180, block.rotation.z);
-                        this.node.addChild(legoClone);
-                    }
-                })
-                
-			}
+                    const pf = this.getBlockGroupByIndex(block.blockGroupType);
+                    const legoClone = instantiate(pf);
+                    legoClone.setPosition(block.position.x, block.position.y, block.position.z);
+                    const bl = legoClone.getChildByName("Block");
+                    if (block.blockGroupType == 5 || block.blockGroupType == 11){
+                        block.rotation.y += 180
+                    } 
+                    bl.setRotationFromEuler(block.rotation.x, block.rotation.y + 180, block.rotation.z);
+                    this.node.addChild(legoClone);
+                })  
+
+                map = jsonAsset.json.levelBlockadesData.blockades;
+                map.forEach(blockades => {
+                    const pf = this.getBlockadesByIndex(blockades.blockType);
+                    const blockadesClone = instantiate(pf);
+                    blockadesClone.setPosition(blockades.position.x, blockades.position.y, blockades.position.z);
+                    blockadesClone.setRotationFromEuler(blockades.rotation.x, blockades.rotation.y + 180, blockades.rotation.z);
+                    this.node.addChild(blockadesClone);
+                })  
+            }
 		});
 	}
 
